f9721d1268da09258f9250c1848398d866cb01ca
Eric Sheffield <ersheff@gmail.com>
2021-11-08 07:50:22 -0500

    asdf

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express"),
   server = require("http").createServer(app),
   io = require("socket.io")(server)
 
+app.use("/")
 server.listen(process.env.PORT || 5000), () => console.log(`Listening...`)
 
 io.on("connection", socket => {
